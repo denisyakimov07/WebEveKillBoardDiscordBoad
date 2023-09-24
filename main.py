188642ee41a9fdd08d1d2f75cf3778cf8d05b5bf
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-24 19:49:38 -0400

    init

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bd import authentication
 
 
 if st.experimental_get_query_params() and authentication(st.experimental_get_query_params()):
-    server_id = st.experimental_get_query_params()['s']
+    server_id = st.experimental_get_query_params()['s'][0]
     print(st.experimental_get_query_params())
     r = requests.get(f"http://172.100.0.100:5000/server_text_channels/{server_id}").text
     st.text(r)
