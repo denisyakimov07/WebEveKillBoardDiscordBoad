cd9db2d69dccddad8f0395196179d9fe28ac8974
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-24 19:42:37 -0400

    init

--- bd.py
+++ bd.py
@@ -1,5 +1,17 @@
 import pymongo
-
+from loguru import logger
 from environment import get_env
 
 client = pymongo.MongoClient(str(get_env().DATA_BASE_URL))
+db: pymongo.database.Database = client.session_url_tokens
+
+def authentication(cred) -> bool:
+    try:
+        server = db.session_url_tokens.find_one({'_id': str(cred['s'][0])})
+        if server and server['url_token'] == str(cred['t'][0]):
+            return True
+        else:
+            return False
+    except Exception as ex:
+        logger.error(f"Cant get server info {ex}")
+
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import requests
 import streamlit as st
 
+from bd import authentication
 
+if st.experimental_get_query_params() and authentication(st.experimental_get_query_params()):
+    server_id = st.experimental_get_query_params()['s']
+    print(st.experimental_get_query_params())
+    r = requests.get(f"http://172.100.0.100:5000/server_text_channels/{server_id}").text
 
-
-print(st.experimental_get_query_params())
+    st.text(r)
