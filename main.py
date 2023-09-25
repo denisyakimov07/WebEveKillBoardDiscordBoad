1bdfae9691be76dadefcc00269b31ff86431111f
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-24 20:26:30 -0400

    init

--- main.py
+++ main.py
@@ -2,10 +2,32 @@ import requests
 import streamlit as st
 
 from bd import authentication
-
+from environment import get_env
 
 if st.experimental_get_query_params() and authentication(st.experimental_get_query_params()):
     server_id = st.experimental_get_query_params()['s'][0]
-    print(st.experimental_get_query_params())
-    r = requests.get(f"http://172.100.0.100:5000/server_text_channels/{server_id}").text
+    headers = {'Authorization': f'Bearer {get_env().DISCORD_BOT_TOKEN}'}
+    r = requests.get(f"http://0.0.0.0:5000/server_text_channels/{server_id}").text
     st.text(r)
+
+
+
+
+grid = st.columns(4)
+
+def add_row(row):
+    with grid[0]:
+        st.text_input('Expense', key=f'input_expense{row}')
+        st.selectbox('Expense',options=['2', "3"])
+    with grid[1]:
+        st.number_input('Amount', key=f'input_amount{row}')
+    with grid[2]:
+        st.number_input('Budget', key=f'input_budget{row}')
+    with grid[3]:
+        st.number_input('Variance', key=f'input_variance{row}',
+                        value = st.session_state[f'input_budget{row}'] \
+                               -st.session_state[f'input_amount{row}'],
+                        disabled=True)
+
+for r in range(4):
+    add_row(r)
